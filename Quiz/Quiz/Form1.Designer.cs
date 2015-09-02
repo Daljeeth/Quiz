5d274b2dfeaa536b35b867c767d1417794a9a7a4
Daljeeth <daljeeths1@gmail.com>
2015-09-02 20:45:37 +0530

    All the Files for the Application.

---
+++ Quiz/Quiz/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Quiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(431, 408);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.DodgerBlue;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.Color.MediumBlue;
+            this.dataGridView1.Location = new System.Drawing.Point(104, 71);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(764, 304);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+           // this.BackgroundImage = global::Quiz.Properties.Resources.quiz_bg3;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(934, 452);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Result";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ Quiz/Quiz/login.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+
+
+
+namespace Quiz
+{
+    public partial class login : Form
+    {
+         public login()
+        {
+            InitializeComponent();
+        }
+
+         private void signin_Click(object sender, EventArgs e) 
+         {
+             try
+             {
+                 string myconn = @"server=192.168.0.1;Database=C:\USERS\DALJEET\DOCUMENTS\VISUAL STUDIO 2013\PROJECTS\QUIZ\QUIZ\DATABASE1.MDF;User Id=Daljeet;password=2015;Connect Timeout=30";
+                 SqlConnection con = new SqlConnection(myconn);
+
+                 string que = "Select * from student_info where username='" + this.username_txt.Text + "' and password='" + this.password_txt.Text + "'";
+                 SqlCommand command = new SqlCommand(que, con);
+
+
+                 SqlDataReader myReader;
+                 con.Open();
+                 myReader = command.ExecuteReader();
+                 int count = 0;
+                 while (myReader.Read())
+                 {
+                     count = count + 1;
+                 }
+                 if (count == 1)
+                 {
+                     welcome f2 = new welcome();
+                     f2.Show();
+                     this.Hide();
+                 }
+                 else if (count > 1)
+                 {
+                     MessageBox.Show("Duplicate username and password.. Access denied");
+
+                 }
+                 else
+                     MessageBox.Show("Username and password is not correct.. Please try again");
+
+
+
+
+                 con.Close();
+
+
+             }
+             catch (Exception Exception)
+             {
+                 MessageBox.Show(Exception.Message);
+             }
+         }
+
+         private void Form1_Load(object sender, EventArgs e)
+         {
+             this.TopMost = true;
+             this.FormBorderStyle = FormBorderStyle.None;
+             this.WindowState = FormWindowState.Maximized;
+             // Set Form's Transperancy 100 %
+             this.Opacity = 0;
+
+             // Start the Timer To Animate Form
+             timer1.Enabled = true;
+         }
+
+        
+
+         private void timer1_Tick(object sender, EventArgs e)
+         {
+             this.Opacity += 0.07;
+         }
+
+         private void exit_Click(object sender, EventArgs e)
+         {
+             Application.Exit();
+         }
+         
+    }
+}
